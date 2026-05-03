353db3018b2f1b5d8c6999b99aa70216967d7724
Z0R0singh077 <z0r0.singh.077@gmail.com>
2026-05-03 05:02:05 +0000

    conversion of datatype

---
+++ 01_Basics/03_conversionOperation.js
@@ -0,0 +1,22 @@
+let score = false
+
+console.log(typeof score)
+console.log(typeof(score))
+
+// number
+let valueInNumber = Number(score);
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+
+// Bollean
+let isLoggedIn = 1
+console.log( typeof isLoggedIn)
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(typeof booleanIsLoggedIn)
+console.log(booleanIsLoggedIn)
+
+// string
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(typeof stringNumber)
+console.log(stringNumber)

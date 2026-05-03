f81022a95e0d13055557659a3ee27b9447e28495
Z0R0singh077 <z0r0.singh.077@gmail.com>
2026-05-03 05:56:28 +0000

    comparisions od data types

---
+++ 01_Basics/04_comparision.js
@@ -0,0 +1,31 @@
+//******************** Comperison **********************//
+
+// basic
+console.log(2>1);
+console.log(2>=1);
+console.log(2<1);
+console.log(2<=1);
+console.log(2==1);
+console.log(2!=1);
+
+// String 
+console.log("2">1);   //true
+console.log("02">1);  // true
+
+// NULL
+console.log(null > 0);
+console.log(null == 0);
+console.log(null < 0);
+console.log(null >=0);
+console.log(null <= 0);
+
+// undefined 
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined <= 0);
+
+// === (Strict comparision)
+// it's the comparision with data types
+
+console.log("2"== 2)  // true
+console.log("2"=== 2) // false

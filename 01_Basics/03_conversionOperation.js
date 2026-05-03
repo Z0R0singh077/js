e053095a2a83e3c5be28028ef23e990d158efc6d
Z0R0singh077 <z0r0.singh.077@gmail.com>
2026-05-03 05:28:08 +0000

    coversion and operator

--- 01_Basics/03_conversionOperation.js
+++ 01_Basics/03_conversionOperation.js
@@ -1,3 +1,5 @@
+// *********************** Conversion ***************************//
+
 let score = false
 
 console.log(typeof score)
@@ -20,3 +22,47 @@ let someNumber = 33
 let stringNumber = String(someNumber)
 console.log(typeof stringNumber)
 console.log(stringNumber)
+
+
+// ********************** Operations ***************************//
+
+let value = 3
+let negvalue = -value
+console.log(negvalue)
+
+// arthimetic operator
+console.log(2+2);
+console.log(2-2);
+console.log(2*3);
+console.log(2/3);
+console.log(2**3);
+console.log(2%3);
+
+// string opertion 
+let str1 = "Zoro"
+let str2 = " Singh"
+str3 = str1 +str2
+console.log(str3);
+
+// some rule for string conversion automally
+console.log(1+"2");
+console.log("1"+2);
+
+console.log("1"+2+2); // after the string use it consider as string
+console.log(1+2+"2"); // before the string it consider as no number which can be add normally 
+
+console.log(true);
+console.log(+true);
+console.log(+" ");
+
+// postfix and prefix 
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
+
+
+
+
+
+
+

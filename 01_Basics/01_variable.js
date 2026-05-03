bf24f88c0f046a76d7929d4d59c7abf3189c26fd
Z0R0singh077 <z0r0.singh.077@gmail.com>
2026-05-03 03:45:48 +0000

    container config done

---
+++ 01_Basics/01_variable.js
@@ -0,0 +1,10 @@
+const accountID =23490
+let accountEmail = "zoro@google.com"
+var accountPassword="123456"
+accountCity= "Goa"
+//accountID =90 // not allowed
+console.log(accountID);
+accountEmail="singh@.com"
+accountPassword="4321"
+accountCity="DELHI"
+console.table([accountEmail,accountID,accountPassword,accountCity])
---
+++ 01_Basics/02_datatypes.js
@@ -0,0 +1,8 @@
+let cName="Nitin"
+let age=28;
+let isLoggedIn= true
+let whether=null;
+let state;
+console.table([cName,age,isLoggedIn,whether,state])
+console.table([typeof("ZORO"),typeof(age),typeof(isLoggedIn),typeof(null),typeof(undefined)])
+console.log(null);
